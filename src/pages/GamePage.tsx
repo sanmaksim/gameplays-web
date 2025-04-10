ecfc7b17f24a794a5fcd4d09eace555b36d37814
Sandra Maksimovic <sanmaksimovic@gmail.com>
2025-04-10 17:30:08 +1000

    Add developer details to game page; fix line endings

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -85,9 +85,9 @@ function GamePage() {
 
   let formattedDate: string;
   if (data && data.original_release_date) {
-      const dateString: string = data.original_release_date;
-      const date: Date = new Date(dateString);
-      formattedDate = new Intl.DateTimeFormat('en-US', { month: 'short', day: '2-digit', year: 'numeric' }).format(new Date(date));
+    const dateString: string = data.original_release_date;
+    const date: Date = new Date(dateString);
+    formattedDate = new Intl.DateTimeFormat('en-US', { month: 'short', day: '2-digit', year: 'numeric' }).format(new Date(date));
   }
 
   return (
@@ -97,21 +97,28 @@ function GamePage() {
       {data && (
         <Card className="my-2">
           <Card.Body className="d-flex">
-            {data.image && <img src={data.image.small_url} alt={data.name} /> }
+            {data.image && <img src={data.image.small_url} alt={data.name} />}
             <div className="d-flex flex-column mx-2">
               <Card.Title>{data.name}</Card.Title>
               <Card.Subtitle className="mb-2 text-muted">Release Date: {formattedDate!}</Card.Subtitle>
               <Card.Text>{data.deck}</Card.Text>
+              <Card.Title>
+                {data.developers && data.developers.length > 1 ? (<>Developers</>) : (<>Developer</>)}
+              </Card.Title>
+              {data.developers && data.developers.map((developer: { name?: string }) => (
+                <li key={developer.name} style={{ listStyle: 'none' }}>{developer.name}</li>
+              ))}
+              <Card.Text></Card.Text>
               <Card.Title>Platforms</Card.Title>
               {data.platforms && data.platforms.map((platform) => (
                 <li key={platform.name} style={{ listStyle: 'none' }}>{platform.name}</li>
               ))}
               {userInfo ? (
                 <div className="mt-auto">
-                  Add to: 
-                  <button onClick={() => createPlay(status.playing)}>Playing</button> | 
-                  <button onClick={() => createPlay(status.played)}>Played</button> | 
-                  <button onClick={() => createPlay(status.wishlist)}>Wishlist</button> | 
+                  Add to:
+                  <button onClick={() => createPlay(status.playing)}>Playing</button> |
+                  <button onClick={() => createPlay(status.played)}>Played</button> |
+                  <button onClick={() => createPlay(status.wishlist)}>Wishlist</button> |
                   <button onClick={() => createPlay(status.backlog)}>Backlog</button>
                 </div>
               ) : (
