abb7a9baa3ef5630907961466c7f7b264fe171e6
Sandra Maksimovic <sanmaksimovic@gmail.com>
2025-05-12 18:36:47 +1000

    Add GET play endpoint for frontend

--- src/slices/playsApiSlice.ts
+++ src/slices/playsApiSlice.ts
@@ -2,10 +2,16 @@ import { apiSlice } from "./apiSlice";
 
 const PLAYS_URL = '/api/plays';
 
+type FormData = {
+    userId: number,
+    gameId: string,
+    status: number
+};
+
 export const playsApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         addPlay: builder.mutation({
-            query: (data) => {
+            query: (data: FormData) => {
                 return {
                     url: `${PLAYS_URL}`,
                     method: 'POST',
@@ -16,8 +22,17 @@ export const playsApiSlice = apiSlice.injectEndpoints({
                     body: data
                 };
             }
+        }),
+        getPlay: builder.mutation({
+            query: (id: string) => {
+                return {
+                    url: `${PLAYS_URL}/${id}`,
+                    method: 'GET',
+                    credentials: 'include'
+                }
+            }
         })
     })
 });
 
-export const { useAddPlayMutation } = playsApiSlice;
+export const { useAddPlayMutation, useGetPlayMutation } = playsApiSlice;
