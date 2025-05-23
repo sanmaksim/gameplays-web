176281534d437150ba4a684891572ba84dcf0d6a
Sandra Maksimovic <sanmaksimovic@gmail.com>
2025-05-23 17:27:24 +1000

    Add basic user games page layout

--- src/pages/GamesPage.tsx
+++ src/pages/GamesPage.tsx
@@ -1,9 +1,60 @@
-import { Container } from "react-bootstrap";
+import { Button, Container, Table } from "react-bootstrap";
+import { useState } from "react";
+import BacklogShelf from "../components/BacklogShelf";
+import PlayedShelf from "../components/PlayedShelf";
+import PlayingShelf from "../components/PlayingShelf";
+import WishlistShelf from "../components/WishlistShelf";
 
 function GamesPage() {
+    const playing = 'Playing';
+    const played = 'Played';
+    const wishlist = 'Wishlist';
+    const backlog = 'Backlog';
+
+    const [activeShelf, setActiveShelf] = useState(playing);
+
+    let shelf;
+    switch (activeShelf) {
+        case playing:
+            shelf = <PlayingShelf />;
+            break;
+        case played:
+            shelf = <PlayedShelf />;
+            break;
+        case wishlist:
+            shelf = <WishlistShelf />;
+            break;
+        case backlog:
+            shelf = <BacklogShelf />;
+            break;
+        default:
+            shelf = null;
+    }
+
     return (
-        <Container>
-            MyGamesPage
+        <Container className="mt-4 d-flex">
+            <Table borderless style={{ width: "150px" }}>
+                <thead>
+                    <tr>
+                        <th>Shelf: {activeShelf}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><Button onClick={() => setActiveShelf(playing)}>Playing</Button></td>
+                    </tr>
+                    <tr>
+                        <td><Button onClick={() => setActiveShelf(played)}>Played</Button></td>
+                    </tr>
+                    <tr>
+                        <td><Button onClick={() => setActiveShelf(wishlist)}>Wishlist</Button></td>
+                    </tr>
+                    <tr>
+                        <td><Button onClick={() => setActiveShelf(backlog)}>Backlog</Button></td>
+                    </tr>
+                </tbody>
+            </Table>
+            {shelf}
         </Container>
     );
 };
