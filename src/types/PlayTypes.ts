177a6ee4433d39782227aa3ad7699ad960d09e38
Sandra Maksimovic <sanmaksimovic@gmail.com>
2025-05-28 18:20:45 +1000

    Add data type for plays returned by user id

--- src/types/PlayTypes.ts
+++ src/types/PlayTypes.ts
@@ -1,3 +1,21 @@
+export interface PlayData {
+    id?: number,
+    name?: string,
+    developers?: [
+        {
+            id?: number,
+            name?: string
+        }
+    ],
+    original_release_date?: string,
+    created_at?: string,
+    hours_played?: number,
+    percentage_completed?: number,
+    last_played_at?: string,
+    status?: number,
+    api_game_id?: number
+}
+
 export interface PlayPayload {
     userId: number,
     gameId: string,
