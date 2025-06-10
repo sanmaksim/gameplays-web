e2ca6915a44fc8cd6e745cf57ca03d9a361d8a20
Sandra Maksimovic <sanmaksimovic@gmail.com>
2025-06-10 16:38:08 +1000

    Update game result page banner colours

--- src/pages/ResultPage.tsx
+++ src/pages/ResultPage.tsx
@@ -137,105 +137,107 @@ function ResultPage() {
         </p>
       )}
       {gameQueryData && (
-        <Card className="my-2">
-          <Card.Body className="d-flex">
-            {gameQueryData.results.image && 
-              <img
-                alt={gameQueryData.results.name}
-                src={gameQueryData.results.image.small_url}
-                width="250px"
-              />
-            }
-            <div className="d-flex flex-column mx-2">
+        <Container>
+          <Card>
+            <Card.Body className="d-flex bg-dark">
+              {gameQueryData.results.image && 
+                <img
+                  alt={gameQueryData.results.name}
+                  src={gameQueryData.results.image.small_url}
+                  width="250px"
+                />
+              }
+              <div className="d-flex flex-column mx-2">
 
-              {/* Game info */}
-              <Card.Title className="display-3 fw-bold">{gameQueryData.results.name}</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">Release Date: {formattedDate!}</Card.Subtitle>
-              <Card.Text className="lead">{gameQueryData.results.deck}</Card.Text>
+                {/* Game info */}
+                <Card.Title className="display-3 fw-bold text-light">{gameQueryData.results.name}</Card.Title>
+                <Card.Subtitle className="mb-2 text-white-50">Release Date: {formattedDate!}</Card.Subtitle>
+                <Card.Text className="lead text-white">{gameQueryData.results.deck}</Card.Text>
 
-              {/* User list control */}
-              {userInfo ? (
-                <>
-                  <Card.Text as={"div"}>
-                    <ActiveButton
-                      index={activeIndex}
-                      label={buttonLabel}
-                      loader={loading}
-                      togglePlay={handleTogglePlay}
-                      showModal={handleShowModal}
-                    />
-                  </Card.Text>
+                {/* User list control */}
+                {userInfo ? (
+                  <>
+                    <Card.Text as={"div"}>
+                      <ActiveButton
+                        index={activeIndex}
+                        label={buttonLabel}
+                        loader={loading}
+                        togglePlay={handleTogglePlay}
+                        showModal={handleShowModal}
+                      />
+                    </Card.Text>
 
-                  <Modal centered show={showModal} onHide={handleCloseModal}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>Choose a shelf for this game</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body className="d-flex flex-column align-items-center justify-content-center mb-2">
-                      <Button
-                        active={activeIndex === Status.Playing}
-                        className="w-50 mb-3"
-                        data-index={Status.Playing}
-                        onClick={(e) => handleTogglePlay(Status.Playing, Number(e.currentTarget.dataset.index))}
-                        variant="outline-primary"
-                      >
-                        {loading && activeIndex === Status.Playing ?
-                          <Loader /> :
-                            activeIndex === Status.Playing ?
-                              Status[Status.Playing] :
-                                `Add to ${Status[Status.Playing]}`
-                        }
-                      </Button>
-                      <Button
-                        active={activeIndex === Status.Played}
-                        className="w-50 mb-3"
-                        data-index={Status.Played}
-                        onClick={(e) => handleTogglePlay(Status.Played, Number(e.currentTarget.dataset.index))}
-                        variant="outline-primary"
-                      >
-                        {loading && activeIndex === Status.Played ?
-                          <Loader /> :
-                            activeIndex === Status.Played ?
-                              Status[Status.Played] :
-                                `Add to ${Status[Status.Played]}`
-                        }
-                      </Button>
-                      <Button
-                        active={activeIndex === Status.Wishlist}
-                        className="w-50 mb-3"
-                        data-index={Status.Wishlist}
-                        onClick={(e) => handleTogglePlay(Status.Wishlist, Number(e.currentTarget.dataset.index))}
-                        variant="outline-primary"
-                      >
-                        {loading && activeIndex === Status.Wishlist ?
-                          <Loader /> :
-                            activeIndex === Status.Wishlist ?
-                              `${Status[Status.Wishlist]}ed` :
-                                `Add to ${Status[Status.Wishlist]}`
-                        }
-                      </Button>
-                      <Button
-                        active={activeIndex === Status.Backlog}
-                        className="w-50"
-                        data-index={Status.Backlog}
-                        onClick={(e) => handleTogglePlay(Status.Backlog, Number(e.currentTarget.dataset.index))}
-                        variant="outline-primary"
-                      >
-                        {loading && activeIndex === Status.Backlog ?
-                          <Loader /> :
-                            activeIndex === Status.Backlog ?
-                              `${Status[Status.Backlog]}ged` :
-                                `Add to ${Status[Status.Backlog]}`
-                        }
-                      </Button>
-                    </Modal.Body>
-                  </Modal>
-                </>
-              ) : (
-                <></>
-              )}
-            </div>
-          </Card.Body>
-          <Card.Body>
+                    <Modal centered show={showModal} onHide={handleCloseModal}>
+                      <Modal.Header closeButton>
+                          <Modal.Title>Choose a shelf for this game</Modal.Title>
+                      </Modal.Header>
+                      <Modal.Body className="d-flex flex-column align-items-center justify-content-center mb-2">
+                        <Button
+                          active={activeIndex === Status.Playing}
+                          className="w-50 mb-3"
+                          data-index={Status.Playing}
+                          onClick={(e) => handleTogglePlay(Status.Playing, Number(e.currentTarget.dataset.index))}
+                          variant="outline-primary"
+                        >
+                          {loading && activeIndex === Status.Playing ?
+                            <Loader /> :
+                              activeIndex === Status.Playing ?
+                                Status[Status.Playing] :
+                                  `Add to ${Status[Status.Playing]}`
+                          }
+                        </Button>
+                        <Button
+                          active={activeIndex === Status.Played}
+                          className="w-50 mb-3"
+                          data-index={Status.Played}
+                          onClick={(e) => handleTogglePlay(Status.Played, Number(e.currentTarget.dataset.index))}
+                          variant="outline-primary"
+                        >
+                          {loading && activeIndex === Status.Played ?
+                            <Loader /> :
+                              activeIndex === Status.Played ?
+                                Status[Status.Played] :
+                                  `Add to ${Status[Status.Played]}`
+                          }
+                        </Button>
+                        <Button
+                          active={activeIndex === Status.Wishlist}
+                          className="w-50 mb-3"
+                          data-index={Status.Wishlist}
+                          onClick={(e) => handleTogglePlay(Status.Wishlist, Number(e.currentTarget.dataset.index))}
+                          variant="outline-primary"
+                        >
+                          {loading && activeIndex === Status.Wishlist ?
+                            <Loader /> :
+                              activeIndex === Status.Wishlist ?
+                                `${Status[Status.Wishlist]}ed` :
+                                  `Add to ${Status[Status.Wishlist]}`
+                          }
+                        </Button>
+                        <Button
+                          active={activeIndex === Status.Backlog}
+                          className="w-50"
+                          data-index={Status.Backlog}
+                          onClick={(e) => handleTogglePlay(Status.Backlog, Number(e.currentTarget.dataset.index))}
+                          variant="outline-primary"
+                        >
+                          {loading && activeIndex === Status.Backlog ?
+                            <Loader /> :
+                              activeIndex === Status.Backlog ?
+                                `${Status[Status.Backlog]}ged` :
+                                  `Add to ${Status[Status.Backlog]}`
+                          }
+                        </Button>
+                      </Modal.Body>
+                    </Modal>
+                  </>
+                ) : (
+                  <></>
+                )}
+              </div>
+            </Card.Body>
+          </Card>
+          <Card.Body className="m-2">
             {/* Game details */}
             {gameEntities.map((gameEntity, index) => (
               <Fragment key={index}>
@@ -251,7 +253,7 @@ function ResultPage() {
               </Fragment>
             ))}
           </Card.Body>
-        </Card>
+        </Container>
       )}
     </Container>
   )
