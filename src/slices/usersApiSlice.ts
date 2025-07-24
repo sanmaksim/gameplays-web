fd8a50f3df1e4519f837dd2da8df0deb2ad398ea
Sandra Maksimovic <sanmaksimovic@gmail.com>
2025-07-24 23:04:15 +1000

    Update users api route url to v1 to reflect server changes

--- src/slices/usersApiSlice.ts
+++ src/slices/usersApiSlice.ts
@@ -1,6 +1,6 @@
 import { apiSlice } from "./apiSlice";
 
-const USERS_URL = '/api/users';
+const USERS_URL = '/api/v1/users';
 
 export const usersApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
