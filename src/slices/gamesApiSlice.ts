04ca8e3c27649fdf86b86fb819591edfa5486aca
Sandra Maksimovic <sanmaksimovic@gmail.com>
2025-08-19 22:26:33 +1000

    Refactor data types and query params

--- src/slices/gamesApiSlice.ts
+++ src/slices/gamesApiSlice.ts
@@ -6,7 +6,7 @@ const GAMES_URL = '/api/games';
 export const gamesApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         getGame: builder.query({
-            query: (id: string) => {
+            query: (id: number) => {
                 return {
                     url: `${GAMES_URL}/${id}`,
                     method: 'GET'
@@ -15,14 +15,14 @@ export const gamesApiSlice = apiSlice.injectEndpoints({
         }),
         search: builder.query({
             query: ({ queryParams, limit = "10" }: GameSearchParams) => {
-                const queryString = new URLSearchParams(queryParams).toString();
                 return {
-                    url: `${GAMES_URL}/search?${queryString}`,
+                    url: `${GAMES_URL}/search`,
                     method: 'GET',
                     headers: {
                         'Content-Type': 'application/json',
                         'Result-Limit': limit
-                    }
+                    },
+                    params: queryParams
                 };
             }
         })
