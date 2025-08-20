1e6d58ce190fbe573cacd98bc9f2264f691c1475
Sandra Maksimovic <sanmaksimovic@gmail.com>
2025-08-20 21:15:38 +1000

    Refactor rendered view to adapt to mobile threshold

--- src/pages/GamesPage.tsx
+++ src/pages/GamesPage.tsx
@@ -1,9 +1,31 @@
 import { Button, Container, Table } from "react-bootstrap";
 import { Status } from "../types/PlayTypes";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ActiveShelf from "../components/ActiveShelf";
 
 function GamesPage() {
+    // toggle mobile screen size threshold
+    const threshold = 768; // 768px for mobile views
+    const [isBelowMobileThreshold, setIsBelowMobileThreshold] = useState(false);
+    
+    useEffect(() => {
+        // set initial state based on current window size
+        const handleResize = () => {
+            if (window.innerWidth < threshold) {
+                setIsBelowMobileThreshold(true);
+            } else {
+                setIsBelowMobileThreshold(false);
+            }
+        };
+    
+        // add resize event listener
+        window.addEventListener('resize', handleResize);
+        handleResize();
+    
+        // cleanup event listener on component unmount
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+    
     const [activeShelf, setActiveShelf] = useState(Status.Playing);
 
     let shelf;
@@ -26,28 +48,47 @@ function GamesPage() {
     // TODO: make menu and active shelf responsive
     return (
         <Container className="mt-4 d-flex">
-            <Table borderless style={{ height: "250px", width: "150px" }}>
-                <thead>
-                    <tr>
-                        <th>Shelf: {Status[activeShelf]}</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td><Button onClick={() => setActiveShelf(Status.Playing)} style={{ width: "80px" }}>Playing</Button></td>
-                    </tr>
-                    <tr>
-                        <td><Button onClick={() => setActiveShelf(Status.Played)} style={{ width: "80px" }}>Played</Button></td>
-                    </tr>
-                    <tr>
-                        <td><Button onClick={() => setActiveShelf(Status.Wishlist)} style={{ width: "80px" }}>Wishlist</Button></td>
-                    </tr>
-                    <tr>
-                        <td><Button onClick={() => setActiveShelf(Status.Backlog)} style={{ width: "80px" }}>Backlog</Button></td>
-                    </tr>
-                </tbody>
-            </Table>
-            {shelf}
+            {isBelowMobileThreshold ? (
+                <div className="d-flex flex-column">
+                    <Table borderless style={{ maxWidth: '510px' }}>
+                        <tbody>
+                            <tr>
+                                <td className="fw-bold">Shelf: {Status[activeShelf]}</td>
+                                <td><Button onClick={() => setActiveShelf(Status.Playing)} style={{ width: "80px" }}>Playing</Button></td>
+                                <td><Button onClick={() => setActiveShelf(Status.Played)} style={{ width: "80px" }}>Played</Button></td>
+                                <td><Button onClick={() => setActiveShelf(Status.Wishlist)} style={{ width: "80px" }}>Wishlist</Button></td>
+                                <td><Button onClick={() => setActiveShelf(Status.Backlog)} style={{ width: "80px" }}>Backlog</Button></td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                    {shelf}
+                </div>
+            ) : (
+                <>
+                    <Table borderless style={{ height: "250px", width: "150px" }}>
+                        <thead>
+                            <tr>
+                                <th>Shelf: {Status[activeShelf]}</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><Button onClick={() => setActiveShelf(Status.Playing)} style={{ width: "80px" }}>Playing</Button></td>
+                            </tr>
+                            <tr>
+                                <td><Button onClick={() => setActiveShelf(Status.Played)} style={{ width: "80px" }}>Played</Button></td>
+                            </tr>
+                            <tr>
+                                <td><Button onClick={() => setActiveShelf(Status.Wishlist)} style={{ width: "80px" }}>Wishlist</Button></td>
+                            </tr>
+                            <tr>
+                                <td><Button onClick={() => setActiveShelf(Status.Backlog)} style={{ width: "80px" }}>Backlog</Button></td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                    {shelf}
+                </>
+            )}
         </Container>
     );
 };
